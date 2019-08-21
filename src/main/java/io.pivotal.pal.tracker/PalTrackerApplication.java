f42cec1a05f3334558fc64a3056d89bb8dc51aef
pal_user <keepitsimple>
2019-08-21 14:37:32 -0400

    added jdbc repository and tests

--- src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
+++ src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
@@ -1,9 +1,10 @@
 package io.pivotal.pal.tracker;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -11,11 +12,8 @@ public class PalTrackerApplication {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
-    @Autowired
-    private InMemoryTimeEntryRepository inMemoryTimeEntryRepository;
-
-    public TimeEntryRepository getTimeEntryRepository() {
-        return inMemoryTimeEntryRepository;
+    @Bean
+    public TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
-
